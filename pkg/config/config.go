0624a4fc3ce4231516ab0a7ae310e2f8c8a1d64b
frrist <forrest@storacha.network>
2025-07-01 11:15:43 -0700

    fixup! fix: remove default PDPServerURL value, since it's optional1

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -45,7 +45,7 @@ type UCANServer struct {
 
 	Port                    uint              `mapstructure:"port" validate:"required,min=1,max=65535" flag:"port"`
 	Host                    string            `mapstructure:"host" validate:"required" flag:"host"`
-	PDPServerURL            string            `mapstructure:"pdp_server_url" validate:"required,url" flag:"pdp-server-url"`
+	PDPServerURL            string            `mapstructure:"pdp_server_url" validate:"url" flag:"pdp-server-url"`
 	PublicURL               string            `mapstructure:"public_url" validate:"omitempty,url" flag:"public-url"`
 	IndexingServiceProof    string            `mapstructure:"indexing_service_proof" flag:"indexing-service-proof"`
 	ProofSet                uint64            `mapstructure:"proof_set" flag:"proof-set"`
