faa10ad4c96ee7f72d3b70d147af2f112c6b212c
frrist <forrest@storacha.network>
2025-07-01 10:33:35 -0700

    fix: don't require node-url flag as it has a default

--- cmd/cli/client/root.go
+++ cmd/cli/client/root.go
@@ -19,7 +19,6 @@ var (
 func init() {
 	Cmd.PersistentFlags().String("node-url", config.DefaultPDPClient.NodeURL, "URL of a Piri node")
 	cobra.CheckErr(viper.BindPFlag("node_url", Cmd.PersistentFlags().Lookup("node-url")))
-	cobra.CheckErr(Cmd.MarkPersistentFlagRequired("node-url"))
 
 	Cmd.AddCommand(ucan.Cmd)
 	Cmd.AddCommand(pdp.Cmd)
