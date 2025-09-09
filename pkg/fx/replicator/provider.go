c772f75fb679b377fe5f2be876ebb699492b5f92
Forrest <forrest@storacha.network>
2025-09-09 12:53:00 -0600

    Implement graceful shutdown of job queue w/ OnFailure method (#225)
    # What
    This change does two thing:
    1. Implements graceful shutdown the of the jobqueue, blocking stop calls
    until all active jobs complete of the stop context is cancel, while
    preventing new jobs from being added during shutdown
    2. Implements an OnFailure option in the Job queue that is called when a
    job exhausts all its retries, this is useful for addressing
    https://github.com/storacha/piri/issues/215 - specifically the case
    mentioned here:
    https://github.com/storacha/piri/pull/216#issuecomment-3253326999

--- pkg/fx/replicator/provider.go
+++ pkg/fx/replicator/provider.go
@@ -4,26 +4,71 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"runtime"
 
+	logging "github.com/ipfs/go-log/v2"
 	"github.com/storacha/go-ucanto/principal"
 	"go.uber.org/fx"
 
 	"github.com/storacha/piri/pkg/config/app"
 	"github.com/storacha/piri/pkg/pdp"
+	"github.com/storacha/piri/pkg/pdp/aggregator/jobqueue"
+	"github.com/storacha/piri/pkg/pdp/aggregator/jobqueue/serializer"
 	"github.com/storacha/piri/pkg/service/blobs"
 	"github.com/storacha/piri/pkg/service/claims"
 	"github.com/storacha/piri/pkg/service/replicator"
+	replicahandler "github.com/storacha/piri/pkg/service/storage/handlers/replica"
 	"github.com/storacha/piri/pkg/store/receiptstore"
 )
 
+var log = logging.Logger("replicator")
+
 var Module = fx.Module("replicator",
 	fx.Provide(
+		ProvideReplicationQueue,
 		fx.Annotate(
 			New,
-			fx.As(new(replicator.Replicator)),
+			fx.As(fx.Self()),                  // provide as concrete type for RegisterReplicationJobs
+			fx.As(new(replicator.Replicator)), // also provide as interface
 		),
 	),
+	fx.Invoke(
+		RegisterReplicationJobs,
+	),
 )
+
+type QueueParams struct {
+	fx.In
+	DB *sql.DB `name:"replicator_db"`
+}
+
+func ProvideReplicationQueue(lc fx.Lifecycle, params QueueParams) (*jobqueue.JobQueue[*replicahandler.TransferRequest], error) {
+	replicationQueue, err := jobqueue.New[*replicahandler.TransferRequest](
+		"replication",
+		params.DB,
+		&serializer.JSON[*replicahandler.TransferRequest]{},
+		jobqueue.WithLogger(log.With("queue", "replication")),
+		// TODO make these values configurable by users, with sane defaults
+		jobqueue.WithMaxRetries(10),
+		jobqueue.WithMaxWorkers(uint(runtime.NumCPU())),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("creating replication queue: %w", err)
+	}
+
+	queueCtx, cancel := context.WithCancel(context.Background())
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			return replicationQueue.Start(queueCtx)
+		},
+		OnStop: func(ctx context.Context) error {
+			cancel()                          // Cancel the Start context first
+			return replicationQueue.Stop(ctx) // Then wait for graceful shutdown
+		},
+	})
+
+	return replicationQueue, nil
+}
 
 type Params struct {
 	fx.In
@@ -34,10 +79,10 @@ type Params struct {
 	Blobs        blobs.Blobs
 	Claims       claims.Claims
 	ReceiptStore receiptstore.ReceiptStore
-	DB           *sql.DB `name:"replicator_db"`
+	Queue        *jobqueue.JobQueue[*replicahandler.TransferRequest]
 }
 
-func New(params Params, lc fx.Lifecycle) (*replicator.Service, error) {
+func New(params Params) (*replicator.Service, error) {
 	r, err := replicator.New(
 		params.ID,
 		params.PDP,
@@ -45,24 +90,18 @@ func New(params Params, lc fx.Lifecycle) (*replicator.Service, error) {
 		params.Claims,
 		params.ReceiptStore,
 		params.Config.UCANService.Services.Upload.Connection,
-		params.DB,
+		params.Queue,
 	)
 	if err != nil {
 		return nil, fmt.Errorf("new replicator: %w", err)
 	}
 
-	lc.Append(fx.Hook{
-		OnStart: func(ctx context.Context) error {
-			// NB(forrest): accept a context todo since the lifecycle hook context has a timeout of 10 sec for starting
-			// a service, we don't want the replicator to timeout here.
-			// Long term fix is for replicator start to accept a contex scoped only for startup operation, or none at all
-			// since it has a dedicated stop method
-			return r.Start(context.TODO())
-		},
-		OnStop: func(ctx context.Context) error {
-			return r.Stop(ctx)
-		},
-	})
-
 	return r, nil
+}
+
+func RegisterReplicationJobs(
+	queue *jobqueue.JobQueue[*replicahandler.TransferRequest],
+	service *replicator.Service,
+) error {
+	return service.RegisterTransferTask(queue)
 }
--- pkg/pdp/aggregator/jobqueue/jobqueue.go
+++ pkg/pdp/aggregator/jobqueue/jobqueue.go
@@ -5,16 +5,22 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"sync"
 	"time"
+
+	logging "github.com/ipfs/go-log/v2"
 
 	"github.com/storacha/piri/pkg/pdp/aggregator/jobqueue/queue"
 	"github.com/storacha/piri/pkg/pdp/aggregator/jobqueue/serializer"
 	"github.com/storacha/piri/pkg/pdp/aggregator/jobqueue/worker"
 )
 
+var log = logging.Logger("jobqueue")
+
 type Service[T any] interface {
-	Start(ctx context.Context)
-	Register(name string, fn func(context.Context, T) error) error
+	Start(ctx context.Context) error
+	Stop(ctx context.Context) error
+	Register(name string, fn func(context.Context, T) error, opts ...worker.JobOption[T]) error
 	Enqueue(ctx context.Context, name string, msg T) error
 }
 
@@ -66,6 +72,14 @@ func WithMaxTimeout(maxTimeout time.Duration) Option {
 type JobQueue[T any] struct {
 	worker *worker.Worker[T]
 	queue  *queue.Queue
+	name   string
+
+	// shutdown management
+	mu          sync.Mutex
+	stopping    bool
+	startCtx    context.Context
+	startCancel context.CancelFunc
+	startWg     sync.WaitGroup
 }
 
 func New[T any](name string, db *sql.DB, ser serializer.Serializer[T], opts ...Option) (*JobQueue[T], error) {
@@ -107,17 +121,94 @@ func New[T any](name string, db *sql.DB, ser serializer.Serializer[T], opts ...O
 	return &JobQueue[T]{
 		queue:  q,
 		worker: w,
+		name:   name,
 	}, nil
 }
 
-func (j *JobQueue[T]) Start(ctx context.Context) {
-	j.worker.Start(ctx)
+func (j *JobQueue[T]) Start(ctx context.Context) error {
+	j.mu.Lock()
+	if j.startCtx != nil {
+		// Already started, this error is almost surly a developer error
+		j.mu.Unlock()
+		return fmt.Errorf("JobQueue[%s] already started", j.name)
+	}
+	j.startCtx, j.startCancel = context.WithCancel(ctx)
+	j.startWg.Add(1)
+	j.mu.Unlock()
+
+	log.Infof("JobQueue[%s] starting", j.name)
+	go func() {
+		defer j.startWg.Done()
+		j.worker.Start(j.startCtx)
+		log.Infof("JobQueue[%s] worker stopped", j.name)
+	}()
+	return nil
 }
 
-func (j *JobQueue[T]) Register(name string, fn func(context.Context, T) error) error {
-	return j.worker.Register(name, fn)
+func (j *JobQueue[T]) Register(name string, fn func(context.Context, T) error, opts ...worker.JobOption[T]) error {
+	j.mu.Lock()
+	if j.startCtx != nil {
+		j.mu.Unlock()
+		return fmt.Errorf("JobQueue[%s] already started, cannot register job on running job queue", j.name)
+	}
+	j.mu.Unlock()
+	return j.worker.Register(name, fn, opts...)
 }
 
 func (j *JobQueue[T]) Enqueue(ctx context.Context, name string, msg T) error {
+	j.mu.Lock()
+	if j.startCtx == nil {
+		j.mu.Unlock()
+		return fmt.Errorf("JobQueue[%s] not started, must start before enqueuing a job", j.name)
+	}
+	if j.stopping {
+		j.mu.Unlock()
+		log.Debugf("JobQueue[%s] rejecting enqueue of %s - queue is stopping", j.name, name)
+		return errors.New("job queue is stopping")
+	}
+	j.mu.Unlock()
 	return j.worker.Enqueue(ctx, name, msg)
+}
+
+func (j *JobQueue[T]) Stop(ctx context.Context) error {
+	j.mu.Lock()
+	if j.startCtx == nil {
+		j.mu.Unlock()
+		return fmt.Errorf("JobQueue[%s] not started, must start before stopping job", j.name)
+	}
+	if j.stopping {
+		j.mu.Unlock()
+		log.Warnf("JobQueue[%s] already stopping, ignoring Stop call", j.name)
+		return errors.New("job queue is already stopping")
+	}
+	j.stopping = true
+	log.Infof("JobQueue[%s] stopping - no new tasks will be accepted", j.name)
+
+	// Cancel the start context to signal worker to stop
+	j.startCancel()
+	j.mu.Unlock()
+
+	log.Infof("JobQueue[%s] waiting for active tasks to complete", j.name)
+
+	// Wait for the worker to finish processing all running tasks
+	done := make(chan struct{})
+	go func() {
+		j.startWg.Wait()
+		close(done)
+	}()
+
+	select {
+	case <-ctx.Done():
+		log.Errorf("JobQueue[%s] stop timeout - some tasks may not have completed gracefully", j.name)
+		return fmt.Errorf("stop timeout: %w", ctx.Err())
+	case <-done:
+		log.Infof("JobQueue[%s] stopped successfully - all tasks completed", j.name)
+		return nil
+	}
+}
+
+// WithOnFailure sets a callback to be invoked only when the job fails after max retries
+// The JobQueue only supports a single OnFailure callback for a job, multiple OnFailure options must not be provided.
+func WithOnFailure[T any](onFailure worker.OnFailureFn[T]) worker.JobOption[T] {
+	return worker.WithOnFailure[T](onFailure)
 }
