66145bde3a0448d723d5a327f095222a3a3e5256
frrist <forrest@storacha.network>
2025-09-15 17:22:53 -0600

    wip

--- cmd/cli/root.go
+++ cmd/cli/root.go
@@ -81,6 +81,7 @@ func init() {
 	rootCmd.AddCommand(delegate.Cmd)
 	rootCmd.AddCommand(initalize.InitCmd)
 	rootCmd.AddCommand(initalize.InstallCmd)
+	rootCmd.AddCommand(initalize.UninstallCmd)
 	rootCmd.AddCommand(client.Cmd)
 	rootCmd.AddCommand(status.Cmd)
 	rootCmd.AddCommand(update.UpdateCmd)
