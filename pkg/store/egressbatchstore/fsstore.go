0f50d94e141f17b7f98c17c0f4f518d51a9eb03a
Vicente Olmedo <vicente@storacha.network>
2025-09-18 15:34:56 +0200

    feat: add EgressBatchStore that batches retrieval receipts

---
+++ pkg/store/egressbatchstore/fsstore.go
@@ -0,0 +1,169 @@
+package egressbatchstore
+
+import (
+	"context"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"sync"
+
+	blocks "github.com/ipfs/go-block-format"
+	"github.com/ipfs/go-cid"
+	"github.com/ipld/go-car/v2/blockstore"
+	cidlink "github.com/ipld/go-ipld-prime/linking/cid"
+	"github.com/multiformats/go-multicodec"
+	"github.com/multiformats/go-multihash"
+	"github.com/storacha/go-libstoracha/capabilities/space/content"
+	"github.com/storacha/go-ucanto/core/receipt"
+	fdm "github.com/storacha/go-ucanto/core/result/failure/datamodel"
+)
+
+const (
+	// DefaultBatchSize is the default maximum size of a receipt batch in bytes.
+	DefaultBatchSize = 100 * 1024 * 1024 // 100MiB
+
+	// currentBatchName is the name of the current batch file.
+	currentBatchName = "egress.car"
+
+	// batchFilePrefix is the prefix for completed batch files.
+	batchFilePrefix = "egress."
+	batchFileSuffix = ".car"
+)
+
+var _ EgressBatchStore = (*fsBatchStore)(nil)
+
+type fsBatchStore struct {
+	mu           sync.Mutex
+	basePath     string
+	curBatchPath string
+	maxBatchSize int64
+}
+
+// NewFSBatchStore creates a new file system based batch receipt store.
+// Batches will be stored in the given basePath.
+// If maxBatchSize is 0, DefaultBatchSize will be used.
+func NewFSBatchStore(basePath string, maxBatchSize int64) (*fsBatchStore, error) {
+	if maxBatchSize <= 0 {
+		maxBatchSize = DefaultBatchSize
+	}
+
+	if err := os.MkdirAll(basePath, 0755); err != nil {
+		return nil, fmt.Errorf("creating egress batch store directory: %w", err)
+	}
+
+	curBatchPath := filepath.Join(basePath, currentBatchName)
+
+	return &fsBatchStore{
+		basePath:     basePath,
+		curBatchPath: curBatchPath,
+		maxBatchSize: maxBatchSize,
+	}, nil
+}
+
+func (s *fsBatchStore) Append(ctx context.Context, rcpt receipt.Receipt[content.RetrieveOk, fdm.FailureModel]) error {
+	if rcpt == nil {
+		return fmt.Errorf("receipt is nil")
+	}
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	rwbs, err := blockstore.OpenReadWrite(s.curBatchPath, nil)
+	if err != nil {
+		return fmt.Errorf("opening current batch for writing: %w", err)
+	}
+
+	rcptArchive := rcpt.Archive()
+	archiveBytes, err := io.ReadAll(rcptArchive)
+	if err != nil {
+		return fmt.Errorf("reading receipt archive: %w", err)
+	}
+
+	cid := rcpt.Root().Link().(cidlink.Link).Cid
+	block, err := blocks.NewBlockWithCid(archiveBytes, cid)
+	if err != nil {
+		return fmt.Errorf("creating receipt block: %w", err)
+	}
+
+	if err := rwbs.Put(ctx, block); err != nil {
+		return fmt.Errorf("adding receipt block to batch: %w", err)
+	}
+
+	if err := rwbs.Finalize(); err != nil {
+		return fmt.Errorf("finalizing batch: %w", err)
+	}
+
+	// send the batch to the egress tracking service if it exceeds the size limit
+	curSize, err := s.currentBatchSize()
+	if err != nil {
+		return fmt.Errorf("checking current batch size: %w", err)
+	}
+	if curSize >= s.maxBatchSize {
+		if err := s.flush(ctx); err != nil {
+			return fmt.Errorf("flushing batch: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (s *fsBatchStore) currentBatchSize() (int64, error) {
+	info, err := os.Stat(s.curBatchPath)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			return 0, nil
+		}
+
+		return 0, fmt.Errorf("checking current batch file: %w", err)
+	}
+
+	return info.Size(), nil
+}
+
+func (s *fsBatchStore) flush(ctx context.Context) error {
+	// Calculate the CID of the current batch
+	f, err := os.Open(s.curBatchPath)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			return nil
+		}
+
+		return fmt.Errorf("opening current batch file: %w", err)
+	}
+	defer f.Close()
+
+	hash := sha256.New()
+	if _, err := io.Copy(hash, f); err != nil {
+		return fmt.Errorf("hashing batch file: %w", err)
+	}
+
+	// error from Encode can be discarded, it's always nil
+	mhBytes, _ := multihash.Encode(hash.Sum(nil), multihash.SHA2_256)
+	mh := multihash.Multihash(mhBytes)
+
+	cidStr := cid.NewCidV1(uint64(multicodec.Car), mh).String()
+	newPath := filepath.Join(s.basePath, batchFilePrefix+cidStr+batchFileSuffix)
+
+	// Rename the file to include the CID
+	if err := os.Rename(s.curBatchPath, newPath); err != nil {
+		return fmt.Errorf("renaming batch file: %w", err)
+	}
+
+	return s.trackEgress(ctx, newPath)
+}
+
+func (s *fsBatchStore) Flush(ctx context.Context) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	return s.flush(ctx)
+}
+
+func (s *fsBatchStore) trackEgress(_ context.Context, _ string) error {
+	// TODO: implement
+	return nil
+}
