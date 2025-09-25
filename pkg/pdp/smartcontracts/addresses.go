2280c3a001abc60f9e847762cd3cc311b7be3a0f
frrist <forrest@storacha.network>
2025-09-24 17:08:08 -0700

    submodule contract code

--- pkg/pdp/smartcontracts/addresses.go
+++ pkg/pdp/smartcontracts/addresses.go
@@ -9,7 +9,8 @@ import (
 )
 
 type PDPContracts struct {
-	PDPVerifier common.Address
+	PDPVerifier  common.Address
+	RecordKeeper common.Address
 }
 
 func Addresses() PDPContracts {
@@ -17,7 +18,10 @@ func Addresses() PDPContracts {
 	// NB(forrest): For now, until we are ready to launch a production network we return
 	// the PDP Service address of the calibration contract, defined at URL above.
 	return PDPContracts{
+		// PDPVerifier contract address
 		PDPVerifier: common.HexToAddress("0x5A23b7df87f59A291C26A2A1d684AD03Ce9B68DC"),
+		// FilecoinWarmStorageService contract address
+		RecordKeeper: common.HexToAddress("0x80617b65FD2EEa1D7fDe2B4F85977670690ed348"),
 	}
 }
 
