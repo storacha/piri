338d022e7ed349cc34439e0b55b17a3cd0ab5fd1
Alan Shaw <alan@storacha.network>
2025-10-16 23:13:46 +0100

    refactor: common handler for blob retrievals

--- pkg/service/retrieval/ucan/blob_retrieve.go
+++ pkg/service/retrieval/ucan/blob_retrieve.go
@@ -2,23 +2,19 @@ package ucan
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"net/http"
 
 	"github.com/storacha/go-libstoracha/capabilities/blob"
 	"github.com/storacha/go-libstoracha/capabilities/space/content"
-	"github.com/storacha/go-libstoracha/digestutil"
 	"github.com/storacha/go-ucanto/core/invocation"
 	"github.com/storacha/go-ucanto/core/receipt/fx"
 	"github.com/storacha/go-ucanto/core/result"
 	"github.com/storacha/go-ucanto/core/result/failure"
-	"github.com/storacha/go-ucanto/did"
 	"github.com/storacha/go-ucanto/principal"
 	"github.com/storacha/go-ucanto/server"
 	"github.com/storacha/go-ucanto/server/retrieval"
 	"github.com/storacha/go-ucanto/ucan"
-	"github.com/storacha/piri/pkg/store"
+	"github.com/storacha/piri/pkg/service/retrieval/handlers/spacecontent"
 	"github.com/storacha/piri/pkg/store/blobstore"
 )
 
@@ -37,52 +33,19 @@ func BlobRetrieve(service BlobRetrievalService) retrieval.Option {
 		retrieval.Provide(
 			blob.Retrieve,
 			func(ctx context.Context, cap ucan.Capability[blob.RetrieveCaveats], inv invocation.Invocation, iCtx server.InvocationContext, request retrieval.Request) (result.Result[blob.RetrieveOk, failure.IPLDBuilderFailure], fx.Effects, retrieval.Response, error) {
-				resource, err := did.Parse(cap.With())
-				if err != nil {
-					return nil, nil, retrieval.Response{}, fmt.Errorf("parsing resource DID: %w", err)
-				}
-				if resource != service.ID().DID() {
+				if cap.With() != service.ID().DID().String() {
 					return result.Error[blob.RetrieveOk, failure.IPLDBuilderFailure](blob.RetrieveError{
 						ErrorName: InvalidResourceErrorName,
-						Message:   fmt.Sprintf("resource is %s not %s", resource, service.ID().DID()),
+						Message:   fmt.Sprintf("resource is %s not %s", cap.With(), service.ID().DID()),
 					}), nil, retrieval.Response{}, nil
 				}
-
-				nb := cap.Nb()
-				digest := nb.Blob.Digest
-				digestStr := digestutil.Format(digest)
-
-				log := log.With(
-					"client", inv.Issuer().DID().String(),
-					"ability", blob.RetrieveAbility,
-					"digest", digestStr,
-				)
-
-				obj, err := service.Blobs().Get(ctx, digest)
+				res, resp, err := spacecontent.Retrieve(ctx, service.Blobs(), inv, cap.Nb().Blob.Digest, blobstore.Range{})
 				if err != nil {
-					if errors.Is(err, store.ErrNotFound) {
-						log.Debugw("blob not found", "status", http.StatusNotFound)
-						notFoundErr := content.NewNotFoundError(fmt.Sprintf("blob not found: %s", digestStr))
-						res := result.Error[blob.RetrieveOk, failure.IPLDBuilderFailure](notFoundErr)
-						resp := retrieval.NewResponse(http.StatusNotFound, nil, nil)
-						return res, nil, resp, nil
-					}
-					log.Errorw("getting blob", "error", err)
-					return nil, nil, retrieval.Response{}, fmt.Errorf("getting blob: %w", err)
+					return nil, nil, retrieval.Response{}, err
 				}
-
-				res := result.Ok[blob.RetrieveOk, failure.IPLDBuilderFailure](blob.RetrieveOk{})
-				status := http.StatusOK
-				contentLength := obj.Size()
-				headers := http.Header{}
-				headers.Set("Content-Length", fmt.Sprintf("%d", contentLength))
-				headers.Set("Content-Type", "application/octet-stream")
-				headers.Set("Cache-Control", "public, max-age=29030400, immutable")
-				headers.Set("Etag", fmt.Sprintf(`"%s"`, digestStr))
-				headers.Set("Vary", "Accept-Encoding")
-				log.Debugw("serving bytes", "status", status, "size", contentLength)
-				resp := retrieval.NewResponse(status, headers, obj.Body())
-				return res, nil, resp, nil
+				return result.MapOk(res, func(o content.RetrieveOk) blob.RetrieveOk {
+					return blob.RetrieveOk{}
+				}), nil, resp, nil
 			},
 		),
 	)
