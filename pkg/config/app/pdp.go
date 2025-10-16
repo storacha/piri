1f886e59315456566e903bd177e2b4322b430506
frrist <forrest@storacha.network>
2025-10-16 14:58:40 -0600

    configure and wire up signer

--- pkg/config/app/pdp.go
+++ pkg/config/app/pdp.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"crypto/ecdsa"
 	"net/url"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -13,4 +14,16 @@ type PDPServiceConfig struct {
 	ContractAddress common.Address
 	// The URL endpoint of a lotus node used for interaction with chain state.
 	LotusEndpoint *url.URL
+	// Signing service configuration used to sign PDP operations
+	SigningServiceConfig SigningServiceConfig
+}
+
+// SigningServiceConfig configures the signing service for PDP operations
+type SigningServiceConfig struct {
+	// URL endpoint for remote signing service (if using HTTP client)
+	Endpoint *url.URL
+	// Private key for in-process signing (if using local signer)
+	// This should be a hex-encoded private key string
+	// NB: this should only be used for development purposes
+	PrivateKey *ecdsa.PrivateKey
 }
