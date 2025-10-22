fd3d7fff914ceb3b51fc40f58ca09ebbaa54ca2e
Vicente Olmedo <vicente@storacha.network>
2025-10-22 22:06:38 +0200

    easier to read default batch size

--- cmd/cli/serve/full.go
+++ cmd/cli/serve/full.go
@@ -108,7 +108,7 @@ func init() {
 	FullCmd.Flags().Int64(
 		"egress-tracker-service-max-batch-size-bytes",
 		// default: 100MiB
-		104857600,
+		100*1024*1024,
 		"Maximum batch size in bytes for egress tracker service. It should be between 10MiB and 1GiB",
 	)
 	cobra.CheckErr(FullCmd.Flags().MarkHidden("egress-tracker-service-max-batch-size-bytes"))
