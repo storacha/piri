462567fe85f5bd10db402614df46c563be369b9a
frrist <forrest@storacha.network>
2025-10-28 12:33:46 -0600

    wip: attempt af dixing add roots

--- pkg/pdp/service/roots_add.go
+++ pkg/pdp/service/roots_add.go
@@ -2,19 +2,16 @@ package service
 
 import (
 	"context"
-	"encoding/hex"
 	"errors"
 	"fmt"
 	"math/big"
-	"math/bits"
 
 	"github.com/ethereum/go-ethereum/common"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
-	"github.com/filecoin-project/go-commp-utils/zerocomm"
+	"github.com/filecoin-project/go-commp-utils/nonffi"
 	commcid "github.com/filecoin-project/go-fil-commcid"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	sha256simd "github.com/minio/sha256-simd"
 	"github.com/samber/lo"
 	"github.com/storacha/piri/pkg/pdp/tasks"
 	"gorm.io/gorm"
@@ -296,52 +293,38 @@ func (p *PDPService) AddRoots(ctx context.Context, id uint64, request []types.Ro
 		return common.Hash{}, err
 	}
 
-	// Calculate global offset across all aggregates in the batch to avoid collisions
-	// when multiple aggregates are submitted in the same transaction.
-	//
-	// IMPORTANT: With dynamic batching, multiple aggregates can be submitted in a single
-	// AddRoots call. Each aggregate would normally start its subroot offsets from 0,
-	// but since they share the same transaction hash, this would violate the UNIQUE
-	// constraint on (proofset_id, add_message_hash, subroot_offset). Using a global
-	// offset ensures each subroot has a unique offset within the transaction.
-	var globalOffset uint64 = 0
-
-	// Log if we're processing a batch
-	if len(request) > 1 {
-		log.Infow("Processing batched AddRoots request",
-			"batch_size", len(request),
-			"proofset_id", id)
-	}
-
+	var totalOffset uint64 = 0
 	// For each AddRootRequest, validate the provided RootCID.
-	for aggregateIdx, addReq := range request {
+	for _, addReq := range request {
 		// Collect pieceInfos for each subroot.
 		pieceInfos := make([]abi.PieceInfo, len(addReq.SubRoots))
-
-		// Log aggregate details if batching
-		if len(request) > 1 {
-			log.Debugw("Processing aggregate in batch",
-				"aggregate_index", aggregateIdx,
-				"root", addReq.Root.String(),
-				"subroot_count", len(addReq.SubRoots),
-				"starting_offset", globalOffset)
-		}
 
 		for i, subCID := range addReq.SubRoots {
 			subInfo, exists := subrootInfoMap[subCID]
 			if !exists {
 				return common.Hash{}, fmt.Errorf("subroot CID %s not found in subroot info map", subCID)
 			}
-			// Set the offset for this subroot using the global offset
-			// This ensures unique offsets across all aggregates in the batch
-			subInfo.SubrootOffset = globalOffset
+			subInfo.SubrootOffset = totalOffset
 			pieceInfos[i] = subInfo.PieceInfo
-			globalOffset += uint64(subInfo.PieceInfo.Size)
+			totalOffset += uint64(subInfo.PieceInfo.Size)
+		}
+
+		// GenerateUnsealedCID requires v1PieceCID, so transform here
+		var v1SubInfos []abi.PieceInfo
+		for _, pi := range pieceInfos {
+			v1PieceCID, err := asPieceCIDv1(pi.PieceCID.String())
+			if err != nil {
+				return common.Hash{}, err
+			}
+			v1SubInfos = append(v1SubInfos, abi.PieceInfo{
+				Size:     pi.Size,
+				PieceCID: v1PieceCID,
+			})
 		}
 
 		// Generate the unsealed CID from the collected piece infos.
 		proofType := abi.RegisteredSealProof_StackedDrg64GiBV1_1
-		generatedCID, err := GenerateUnsealedCID(proofType, pieceInfos)
+		generatedCID, err := nonffi.GenerateUnsealedCID(proofType, v1SubInfos)
 		if err != nil {
 			return common.Hash{}, fmt.Errorf("failed to generate RootCID: %w", err)
 		}
@@ -383,104 +366,26 @@ func (p *PDPService) AddRoots(ctx context.Context, id uint64, request []types.Ro
 			return common.Hash{}, fmt.Errorf("computing height and padding: %w", err)
 		}
 		// NB: defined here: https://github.com/FilOzone/pdp/blob/main/src/PDPVerifier.sol#L44
-		// as MAX_PIECE_SIZE_LOG2 = 50
-		// TODO: there is an accessor method on the verifier for getting this value, add to interface
-		if height > 50 {
+		maxPieceSizeLog2, err := p.verifierContract.MaxPieceSizeLog2(ctx)
+		if err != nil {
+			return common.Hash{}, err
+		}
+		if uint64(height) > maxPieceSizeLog2.Uint64() {
 			return common.Hash{}, fmt.Errorf("invalid height: %d", height)
 		}
 
-		// REVIEW: I am mega unsure of these code changes, but they do "pass" with the PDP Verifier contract.
-		// TODO(forrest): since migrating to the new contract, rod states there is an error
-		// in the curio size logic, context here: https://github.com/filecoin-project/curio/issues/650
-		// in commit 91aff56959407ec83171ef73d48c51fed8afb4c7 of curio
-		// Filecoin Team hash stated:
-		// subpieces are broken on our branch (called `rename` in Curio, don’t use it if you’re relying on subpieces.
-		// In fact, PieceCIDv2 isn’t quite working on our branch with this being an outstanding problem that’s currently
-		// biting us: https://github.com/filecoin-project/curio/issues/650; the not-quite-comprehensive switch to
-		// PieceCIDv2 hasn’t been a success, but we don’t really want to be changing the db schema just for that so we
-		// compromised and deferred the proper job to mkv2.
-
-		// Get total size by summing up the sizes of subroots
-		// We track both padded and unpadded sizes:
-		// - Padded sizes are used for smart contract operations
-		// - Unpadded sizes are used to validate against PieceCIDV2's embedded rawSize
 		var totalSize uint64 = 0
-		var totalUnpaddedSize uint64 = 0
 		var prevSubrootSize = subrootInfoMap[addRootReq.SubRoots[0]].PieceInfo.Size
 		for i, subrootEntry := range addRootReq.SubRoots {
 			subrootInfo := subrootInfoMap[subrootEntry]
 			if subrootInfo.PieceInfo.Size > prevSubrootSize {
+				// implies a bad request
 				return common.Hash{}, fmt.Errorf("subroots must be in descending order of size, root %d %s is larger than prev subroot %s", i, subrootEntry, addRootReq.SubRoots[i-1])
 			}
 
-			prevSubrootSize = subrootInfo.PieceInfo.Size
 			paddedSize := uint64(subrootInfo.PieceInfo.Size)
-			// CRITICAL FIX: Use the actual raw size from the database, not .Unpadded() on padded size
-			// The padded size includes both FR32 padding AND zero-padding to power-of-2
-			// So calling .Unpadded() only removes FR32 padding, leaving zero-padding... SOOOooOOoo FUN! :') \s
-			unpaddedSize := subrootInfo.RawSize
-			log.Infow("Subroot size details",
-				"subrootCID", subrootEntry,
-				"paddedSize", paddedSize,
-				"paddedSizeMod32", paddedSize%32,
-				"unpaddedSize", unpaddedSize,
-				"unpaddedSizeMod32", unpaddedSize%32,
-				"rawSizeFromDB", subrootInfo.RawSize)
 			totalSize += paddedSize
-			totalUnpaddedSize += unpaddedSize
 		}
-
-		// Log debug information
-		log.Infow("Root data details",
-			"rootCID", addRootReq.Root,
-			"cidBytesLen", len(rootCID.Bytes()),
-			"totalSize", totalSize,
-			"totalSizeMod32", totalSize%32,
-			"totalUnpaddedSize", totalUnpaddedSize,
-			"totalUnpaddedSizeMod32", totalUnpaddedSize%32,
-			"subrootCount", len(addRootReq.SubRoots))
-
-		// Check if the rawSize in the PieceCIDv2 matches the totalUnpaddedSize of the subPieces
-		// Note: PieceCIDv2's rawSize represents unpadded data size, so we compare against totalUnpaddedSize
-		// For now, just warn and reconstruct below instead of rejecting
-		if rawSize != totalUnpaddedSize {
-			log.Warnw("PieceCIDv2 embedded rawSize mismatch - will reconstruct with correct size",
-				"embeddedRawSize", rawSize,
-				"calculatedTotalUnpaddedSize", totalUnpaddedSize,
-				"rootCID", rootCID.String())
-			// FIXME TODO: this assertionis failing
-			// return common.Hash{}, fmt.Errorf("raw size miss-match: expected %d, got %d", rawSize, totalUnpaddedSize)
-		}
-
-		// Defensively reconstruct the PieceCIDv2 to ensure it has the correct embedded size
-		// Even though the hash validated above, we want to ensure the size encoding is correct
-		commitment, extractedPayloadSize, err := commcid.PieceCidV2ToDataCommitment(rootCID)
-		if err != nil {
-			return common.Hash{}, fmt.Errorf("failed to extract commitment from PieceCIDv2: %w", err)
-		}
-
-		// Additional validation: the extracted payload size should match totalUnpaddedSize
-		if extractedPayloadSize != totalUnpaddedSize {
-			log.Warnw("PieceCIDv2 embedded size mismatch - will reconstruct with correct size",
-				"extractedPayloadSize", extractedPayloadSize,
-				"totalUnpaddedSize", totalUnpaddedSize,
-				"rootCID", rootCID.String())
-		}
-		reconstructedPieceCidV2, err := commcid.DataCommitmentToPieceCidv2(commitment, totalUnpaddedSize)
-		if err != nil {
-			return common.Hash{}, fmt.Errorf("failed to reconstruct PieceCIDv2: %w", err)
-		}
-
-		// Log the reconstruction for debugging
-		log.Infow("Reconstructed PieceCIDv2 for validation",
-			"original", addRootReq.Root.String(),
-			"reconstructed", reconstructedPieceCidV2.String(),
-			"match", addRootReq.Root.Equals(reconstructedPieceCidV2),
-			"totalUnpaddedSize", totalUnpaddedSize,
-			"originalRawSize", rawSize)
-
-		// Use the reconstructed CIDv2 to ensure correct size is embedded
-		rootCID = reconstructedPieceCidV2
 
 		// Prepare RootData for Ethereum transaction using the generated binding type
 		rootData := smartcontracts.CidsCid{
@@ -504,14 +409,6 @@ func (p *PDPService) AddRoots(ctx context.Context, id uint64, request []types.Ro
 	if err != nil {
 		return common.Hash{}, fmt.Errorf("failed to get next piece ID for dataset %d: %w", id, err)
 	}
-
-	log.Infow("AddPieces signing parameters",
-		"proofSetID", proofSetID,
-		"clientDataSetId", datasetInfo.ClientDataSetId,
-		"datasetPayer", datasetInfo.Payer,
-		"configuredPayer", smartcontracts.PayerAddress,
-		"firstAdded(nextPieceId)", nextPieceId,
-		"pieceCount", len(pieceDataArray))
 
 	// Convert pieceDataArray to [][]byte for signing
 	pieceDataBytes := make([][]byte, len(pieceDataArray))
@@ -544,13 +441,6 @@ func (p *PDPService) AddRoots(ctx context.Context, id uint64, request []types.Ro
 		return common.Hash{}, fmt.Errorf("failed to encode extraData: %w", err)
 	}
 
-	// Pack the method call data
-	log.Infow("AddPieces contract parameters",
-		"proofSetID", proofSetID,
-		"pieceCount", len(pieceDataArray),
-		"firstPieceCID", hex.EncodeToString(pieceDataArray[0].Data),
-		"firstPieceSignedCID",
-		hex.EncodeToString(pieceDataBytes[0]))
 	// listener must be empty address for datasets that already exist, thus 3rd argument.
 	data, err := abiData.Pack("addPieces", proofSetID, common.Address{}, pieceDataArray, extraDataBytes)
 	if err != nil {
@@ -631,124 +521,4 @@ func (p *PDPService) AddRoots(ctx context.Context, id uint64, request []types.Ro
 
 	log.Infow("AddRoots transaction confirmed successfully", "txHash", txHash.Hex(), "proofSetID", proofSetID)
 	return txHash, nil
-}
-
-// Review: Code below is a copy of github.com/filecoin-project/go-commp-utils/nonffi with support for PieceCIDv2
-// mega unsure
-
-type stackFrame struct {
-	size  uint64
-	commP []byte
-}
-
-func GenerateUnsealedCID(proofType abi.RegisteredSealProof, pieceInfos []abi.PieceInfo) (cid.Cid, error) {
-	spi, found := abi.SealProofInfos[proofType]
-	if !found {
-		return cid.Undef, fmt.Errorf("unknown seal proof type %d", proofType)
-	}
-	if len(pieceInfos) == 0 {
-		return cid.Undef, errors.New("no pieces provided")
-	}
-
-	maxSize := uint64(spi.SectorSize)
-
-	todo := make([]stackFrame, len(pieceInfos))
-
-	// sancheck everything
-	for i, p := range pieceInfos {
-		if p.Size < 128 {
-			return cid.Undef, fmt.Errorf("invalid Size of PieceInfo %d: value %d is too small", i, p.Size)
-		}
-		if uint64(p.Size) > maxSize {
-			return cid.Undef, fmt.Errorf("invalid Size of PieceInfo %d: value %d is larger than sector size of SealProofType %d", i, p.Size, proofType)
-		}
-		if bits.OnesCount64(uint64(p.Size)) != 1 {
-			return cid.Undef, fmt.Errorf("invalid Size of PieceInfo %d: value %d is not a power of 2", i, p.Size)
-		}
-
-		cp, _, err := commcid.PieceCidV2ToDataCommitment(p.PieceCID)
-		if err != nil {
-			return cid.Undef, fmt.Errorf("invalid PieceCid for PieceInfo %d: %w", i, err)
-		}
-		todo[i] = stackFrame{size: uint64(p.Size), commP: cp}
-	}
-
-	// reimplement https://github.com/filecoin-project/rust-fil-proofs/blob/380d6437c2/filecoin-proofs/src/pieces.rs#L85-L145
-	stack := append(
-		make(
-			[]stackFrame,
-			0,
-			32,
-		),
-		todo[0],
-	)
-
-	for _, f := range todo[1:] {
-
-		// pre-pad if needed to balance the left limb
-		for stack[len(stack)-1].size < f.size {
-			lastSize := stack[len(stack)-1].size
-
-			stack = reduceStack(
-				append(
-					stack,
-					stackFrame{
-						size:  lastSize,
-						commP: zeroCommForSize(lastSize),
-					},
-				),
-			)
-		}
-
-		stack = reduceStack(
-			append(
-				stack,
-				f,
-			),
-		)
-	}
-
-	for len(stack) > 1 {
-		lastSize := stack[len(stack)-1].size
-		stack = reduceStack(
-			append(
-				stack,
-				stackFrame{
-					size:  lastSize,
-					commP: zeroCommForSize(lastSize),
-				},
-			),
-		)
-	}
-
-	if stack[0].size > maxSize {
-		return cid.Undef, fmt.Errorf("provided pieces sum up to %d bytes, which is larger than sector size of SealProofType %d", stack[0].size, proofType)
-	}
-
-	// TODO probably need to be pieceCIDv2
-	return commcid.PieceCommitmentV1ToCID(stack[0].commP)
-}
-
-var s256 = sha256simd.New()
-
-func zeroCommForSize(s uint64) []byte { return zerocomm.PieceComms[bits.TrailingZeros64(s)-7][:] }
-
-func reduceStack(s []stackFrame) []stackFrame {
-	for len(s) > 1 && s[len(s)-2].size == s[len(s)-1].size {
-
-		s256.Reset()
-		s256.Write(s[len(s)-2].commP)
-		s256.Write(s[len(s)-1].commP)
-		d := s256.Sum(make([]byte, 0, 32))
-		d[31] &= 0b00111111
-
-		s[len(s)-2] = stackFrame{
-			size:  2 * s[len(s)-2].size,
-			commP: d,
-		}
-
-		s = s[:len(s)-1]
-	}
-
-	return s
 }
