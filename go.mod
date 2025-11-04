140d8fe8937fff3d653a330ab94408f047fcebea
ash <alan@storacha.network>
2025-11-04 15:32:43 +0000

    fix: retrieval service trusts attestations issued by the upload service (#326)
    This PR allows the Piri retrieval service to trust attestations issued
    by the upload service.
    
    The upload service attests to a absentee delegation from a `did:mailto:`
    in order to allow users to have multiple private keys but use a email as
    a means of sharing authority.
    
    Depends on:
    
    * https://github.com/storacha/go-ucanto/pull/79
    * https://github.com/storacha/go-libstoracha/pull/64

--- go.mod
+++ go.mod
@@ -63,8 +63,8 @@ require (
 	github.com/spf13/viper v1.21.0
 	github.com/storacha/delegator v0.0.2-0.20251027182137-7d26b5ae9a70
 	github.com/storacha/filecoin-services/go v0.0.1
-	github.com/storacha/go-libstoracha v0.3.3
-	github.com/storacha/go-ucanto v0.7.0
+	github.com/storacha/go-libstoracha v0.4.0
+	github.com/storacha/go-ucanto v0.7.1
 	github.com/storacha/piri-signing-service v0.0.1
 	github.com/stretchr/testify v1.11.1
 	github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7
--- pkg/fx/retrieval/ucan/provider.go
+++ pkg/fx/retrieval/ucan/provider.go
@@ -3,16 +3,23 @@ package ucan
 import (
 	"fmt"
 
+	logging "github.com/ipfs/go-log/v2"
 	"github.com/labstack/echo/v4"
+	ucancap "github.com/storacha/go-libstoracha/capabilities/ucan"
+	"github.com/storacha/go-ucanto/core/delegation"
 	"github.com/storacha/go-ucanto/principal"
 	ucanserver "github.com/storacha/go-ucanto/server"
 	ucanretrieval "github.com/storacha/go-ucanto/server/retrieval"
-	"go.uber.org/fx"
+	"github.com/storacha/go-ucanto/ucan"
 
+	"github.com/storacha/piri/pkg/config/app"
 	echofx "github.com/storacha/piri/pkg/fx/echo"
 	"github.com/storacha/piri/pkg/fx/retrieval/ucan/handlers"
 	"github.com/storacha/piri/pkg/service/retrieval"
+	"go.uber.org/fx"
 )
+
+var log = logging.Logger("fx/retrieval/ucan")
 
 type Handler struct {
 	ucanServer ucanserver.ServerView[ucanretrieval.Service]
@@ -34,11 +41,42 @@ type Params struct {
 	fx.In
 
 	ID      principal.Signer
+	Config  app.AppConfig
 	Options []ucanretrieval.Option `group:"ucan_retrieval_options"`
 }
 
 func NewHandler(p Params) (*Handler, error) {
-	ucanSvr, err := ucanretrieval.NewServer(p.ID, p.Options...)
+	// Create a local delegation to the upload service that allows it to issue
+	// attestations. When the validator sees this delegation, it will accept
+	// attestations issued by the upload service.
+	attestDlg, err := delegation.Delegate(
+		p.ID,
+		p.Config.UCANService.Services.Upload.Connection.ID(),
+		[]ucan.Capability[ucan.NoCaveats]{
+			ucan.NewCapability(
+				ucancap.AttestAbility,
+				p.ID.DID().String(),
+				ucan.NoCaveats{},
+			),
+		},
+		delegation.WithNoExpiration(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	options := []ucanretrieval.Option{
+		ucanretrieval.WithAuthorityProofs(attestDlg),
+		ucanretrieval.WithErrorHandler(func(err ucanserver.HandlerExecutionError[any]) {
+			l := log.With("error", err.Error())
+			if s := err.Stack(); s != "" {
+				l = l.With("stack", s)
+			}
+			l.Error("ucan retrieval handler execution error")
+		}),
+	}
+	options = append(options, p.Options...)
+	ucanSvr, err := ucanretrieval.NewServer(p.ID, options...)
 	if err != nil {
 		return nil, fmt.Errorf("creating ucan retrieval server: %w", err)
 	}
