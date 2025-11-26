ea72746662df1fc44bba5eb25fab91efa4b7af05
Vicente Olmedo <vicente@storacha.network>
2025-11-26 18:55:47 +0100

    fix: adjust client config

--- pkg/config/client.go
+++ pkg/config/client.go
@@ -1,9 +1,13 @@
 package config
 
 type Client struct {
-	Identity    IdentityConfig    `mapstructure:"identity"`
-	API         API               `mapstructure:"api"`
-	UCANService UCANServiceConfig `mapstructure:"ucan" toml:"ucan"`
+	Identity IdentityConfig `mapstructure:"identity"`
+	API      API            `mapstructure:"api"`
+	UCAN     UCANConfig     `mapstructure:"ucan" toml:"ucan"`
+}
+
+type UCANConfig struct {
+	ProofSetID uint64 `mapstructure:"proof_set" toml:"proof_set"`
 }
 
 func (c Client) Validate() error {
